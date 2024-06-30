d079f9b06749e8201fa3cf20ada115847893b146
MrInmortal434 <118313004+MrInmortal@users.noreply.github.com>
2024-06-29 21:08:05 -0400

    Add files via upload

---
+++ proyecto.Infrastructure/Interfaces/IActivityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Foundation.Domain.Entities;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Foundation.Infrastructure.Interfaces
+{
+    public interface IActivityRepository
+    {
+        Activity GetActivityById(int activityId);
+        IEnumerable<Activity> GetAllActivities();
+        void AddActivity(Activity activity);
+        void UpdateActivity(Activity activity);
+        void DeleteActivity(int activityId);
+    }
+}
